e21e43e36bd42991802c963ebc1e17635bb42b1d
Ahmed-Alnabhan <amn5948@rit.edu>
2017-07-27 22:46:40 -0500

    fix: Resolve merge conflict in string.xml and MainActivity.java

--- app/src/main/java/com/elearnna/myappportfolio/MainActivity.java
+++ app/src/main/java/com/elearnna/myappportfolio/MainActivity.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.elearnna.myappportfolio;
 
 import android.content.Intent;
@@ -73,72 +72,3 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 }
-||||||| merged common ancestors
-=======
-package com.elearnna.myappportfolio;
-
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toast;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-    }
-
-    /**
-     * This method displays a toast message explaining what each button does.
-     * @param view The clicked button
-     */
-    public void launchAProgect(View view){
-        // gets the tag of the clicked button
-        String projectName = (String) view.getTag();
-        String text = "This button will launch my  " + projectName + " app.";
-        Toast toast = Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT);
-        toast.show();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-}
->>>>>>> b9a52201ca82a1c9b6b55af74c64633fc5c3d7e0
