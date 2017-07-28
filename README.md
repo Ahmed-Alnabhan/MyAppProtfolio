2ad1136c93a99a9106b3be254e900bb6985d2d37
Ahmed-Alnabhan <amn5948@rit.edu>
2017-07-27 22:28:03 -0500

    feat: Connect a project button to the corresponding app

---
+++ README.md
@@ -0,0 +1,3 @@
+# MyAppProtfolio
+
+An Android app that shows off the projects created as part of the Android Developer Nanodegree program.
---
+++ app/src/main/java/com/elearnna/myappportfolio/MainActivity.java
@@ -0,0 +1,74 @@
+package com.elearnna.myappportfolio;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String PACKAGE_START_PART = "com.";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+    }
+
+    /**
+     * This method displays a toast message explaining what each button does.
+     * @param view The clicked button
+     */
+    public void launchAProgect(View view){
+        // gets the tag of the clicked button
+        String projectTag = (String) view.getTag();
+        String packagePrefix = projectTag.substring(0,4);
+        if (packagePrefix.equals(PACKAGE_START_PART)){
+            Intent launchIntent = getPackageManager().getLaunchIntentForPackage(projectTag);
+            startActivity(launchIntent);
+        } else {
+            String text = "This button will launch my  " + projectTag + " app.";
+            Toast toast = Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
